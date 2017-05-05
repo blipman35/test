939d891ba4d9a5378089a9065821600b10eb8aa1
blipman35 <blipman35@gmail.com>
2017-05-04 21:10:56 -0700

    finished game, need to add documentation

--- main.py
+++ main.py
@@ -11,25 +11,30 @@ c = Player(b.board_values)
 bv = b.board_values
 computer = Computer(c,b,bv)
 
-while movenum < 9 and winner == "":
-    p.playermove()
-    print("")
-    print("Your move")
-    b.drawboard()
-    movenum += 1
-    if computer.testwin('x') == 'yes':
-        winner = 'human'
-
-    if movenum < 9 and winner == "":
-        computer.computermove('o', 'x')
+print("Welcome to Ben's AI tic tac toe game.")
+print("You are X and the computer is O.")
+print("You will enter a move number from 1-9 starting at the top left of the board")
+start = input("Press 1 to start:")
+if start == '1':
+    while movenum < 9 and winner == "":
+        p.playermove()
         print("")
-        print("My move")
+        print("Your move")
         b.drawboard()
         movenum += 1
-        if computer.testwin('o') == 'yes':
-            winner = 'computer'
+        if computer.testwin('x') == 'yes':
+            winner = 'human'
 
-if winner == "":
-    print("It is a tie!")
-else:
-    print("The winner is " + str(winner))
+        if movenum < 9 and winner == "":
+            computer.computermove('o', 'x')
+            print("")
+            print("My move")
+            b.drawboard()
+            movenum += 1
+            if computer.testwin('o') == 'yes':
+                winner = 'computer'
+
+    if winner == "":
+        print("It is a tie!")
+    else:
+        print("The winner is " + str(winner))
